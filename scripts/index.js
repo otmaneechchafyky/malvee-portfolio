355b841c8e875e26b5bdd4d5d8056d1d577b000a
unknown <ismayito1@gmail.com>
2023-03-06 16:19:14 +0300

    Add event listeners to menu links item

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,21 @@
 const mobile_menu_btn = document.getElementById("breadcrumb-icon");
 const mobile_menu = document.getElementById("mobile-menu");
 const close_menu_btn = document.getElementById("close-menu-btn");
+const menu_links = document.getElementsByClassName("menu-items");
 
+// menu_links.map((link) => {
+//   console.log(link);
+// });
 
-mobile_menu_btn.addEventListener("click", function(){
-    mobile_menu.classList.add("visible");
-})
+for (let i = 0; i < menu_links.length; i++) {
+  menu_links[i].addEventListener("click", function (e) {
+    mobile_menu.classList.remove("visible");
+  });
+}
+mobile_menu_btn.addEventListener("click", function () {
+  mobile_menu.classList.add("visible");
+});
 
-close_menu_btn.addEventListener("click", function(){
+close_menu_btn.addEventListener("click", function () {
   mobile_menu.classList.remove("visible");
-})
+});
